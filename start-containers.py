948462ed57bf92e1397e309c5b0cccc0dd188b34
Brendan Abolivier <contact@brendanabolivier.com>
2016-02-13 00:05:14 +0100

    Added ability to stop and remove containers (for systemctl stop)

--- start-containers.py
+++ start-containers.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 from docker import Client
-import os
+import os, sys
 import json
 
 json_file = "/etc/infra.d/sites.json"
@@ -15,27 +15,51 @@ config_file = ""
 container_image = ""
 out = {}
 
-for site in sites:
-    site_name = site["name"]
-    config_file = "/etc/infra.d/sites/" +  site["name"] + ".caddyfile"
-    container_image = "abiosoft/caddy:" + site["image"]
-    print("Working on site", site_name)
-    if os.path.exists(config_file):
-        print("Starting container for", site_name)
-        out = cli.create_container(
-            image=container_image, 
-            name=site_name, 
-            volumes=["/etc/Caddyfile"],
-            host_config=cli.create_host_config(binds={
-                config_file: {
-                    'bind': '/etc/Caddyfile',
-                    'mode': 'ro',
-                }
-            })
-        )
-        if out.get("Warnings") is None:
-            cli.start(out.get("Id"))
-            print("Container", out.get("Id"), "created and started.")
-    else:
-        print("File", config_file,"doesn't exist.")
+caddyImages = {}
+caddyContainers = []
 
+if sys.argv[1] == "--start":
+    for site in sites:
+        site_name = site["name"]
+        config_file = "/etc/infra.d/sites/" +  site["name"] + ".caddyfile"
+        container_image = "abiosoft/caddy:" + site["image"]
+        print("Working on site", site_name)
+        if os.path.exists(config_file):
+            print("Starting container for", site_name)
+            out = cli.create_container(
+                image=container_image, 
+                name=site_name, 
+                volumes=["/etc/Caddyfile"],
+                host_config=cli.create_host_config(binds={
+                    config_file: {
+                        'bind': '/etc/Caddyfile',
+                        'mode': 'ro',
+                    }
+                })
+            )
+            if out.get("Warnings") is None:
+                cli.start(out.get("Id"))
+                print("Container", out.get("Id"), "created and started.")
+        else:
+            print("File", config_file,"doesn't exist.")
+
+elif sys.argv[1] == "--stop":
+    # Yup, dirty copy/paste from start-proxy. Should package it someday.
+    images = cli.images()
+
+    for image in images:
+        if "caddy:latest" in image["RepoTags"][0]:
+            caddyImages["default"] = image["Id"]
+        elif "caddy:php" in image["RepoTags"][0]:
+            caddyImages["php"] = image["Id"]
+
+    containers = cli.containers()
+
+    for container in containers:
+        if container["ImageID"] == caddyImages["default"] or container["ImageID"] == caddyImages["php"]:
+            name = container["Names"][0][1:]
+            # Let's manage the proxy with its own service
+            if name != "proxy":
+                print("Stopping", name)
+                cli.stop(container=container.get("Id"), timeout=5)
+                cli.remove_container(container=container.get("Id"))
