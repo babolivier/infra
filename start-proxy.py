c7f30ab891eea0f849683a6f38ca77c4e46273cb
Brendan Abolivier <contact@brendanabolivier.com>
2016-02-17 21:06:17 +0100

    Now using the Docker API

--- start-proxy.py
+++ start-proxy.py
@@ -4,6 +4,8 @@ from docker import Client
 import os
 import pprint
 
+config_file = "/etc/infra.d/proxy.caddyfile"
+
 caddyImages = {}
 caddyContainers = []
 
@@ -26,11 +28,36 @@ for container in containers:
         if name != "proxy":
             caddyContainers.append(name)
 
-link = ""
-
+links={}
 for name in caddyContainers:
-    link = link + "--link " + name + ":" + name + " "
+    links[name] = name
 
-command = "/usr/bin/docker run " + link + " -p 80:80 -p 443:443 -v /root/.caddy:/root/.caddy -v /etc/infra.d/proxy.caddyfile:/etc/Caddyfile --name proxy abiosoft/caddy:latest"
+# Creating the specific HostConfig for the proxy
+host_config = cli.create_host_config(links=links, binds={
+    config_file: {
+        'bind': '/etc/Caddyfile',
+        'mode': 'ro'
+    },
+    "/root/.caddy": {
+        'bind': '/root/.caddy',
+        'mode': 'rw'
+    }
+}, port_bindings={
+    80: 80,
+    443: 443
+})
 
-os.system(command)
+container = cli.create_container(
+    image="abiosoft/caddy:latest",
+    name="proxy",
+    host_config=host_config
+)
+
+if container.get("Warnings") is None:
+    cli.start(container.get("Id"))
+    print("Proxy successfully started with id", container.get("Id"))
+
+
+#command = "/usr/bin/docker run " + link + " -p 80:80 -p 443:443 -v /root/.caddy:/root/.caddy -v /etc/infra.d/proxy.caddyfile:/etc/Caddyfile --name proxy abiosoft/caddy:latest"
+
+#os.system(command)
